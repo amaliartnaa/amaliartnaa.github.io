e9e0d197acc6673d3c1d7cdaf13f8865464a0613
Ratna Amalia Azzahra <amaliaratna41@gmail.com>
2023-10-22 20:09:13 +0700

    update ke 5

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
 function validateForm() {
     // Dapatkan nilai input
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var subject = document.getElementById('subject').value;
-    var message = document.getElementById('message').value;
+    var name = document.getElementById('name');
+    var email = document.getElementById('email');
+    var subject = document.getElementById('subject');
+    var message = document.getElementById('message');
 
     // Validasi input
-    if (name === '' || email === '' || subject === '' || message === '') {
-        alert('Semua kolom harus diisi.');
-        return false; // Form tidak akan disubmit
-    } else if (!isValidEmail(email)) {
-        alert('Email tidak valid.');
-        return false; // Form tidak akan disubmit
-    } else {
-        // Semua validasi terpenuhi, form akan disubmit
-        return true;
+
+
+    if(name.value == "") {
+        alert("Nama harus diisi!");
+        name.placeholder = "mohon isi nama"
+        name.focus();
+        return false;
     }
+
+    if(email.value == "") {
+        alert("Email harus diisi!");
+        email.placeholder = "mohon isi email";
+        email.focus();
+        return false;
+    }
+
+    if(!isValidEmail(email.value)) {
+        alert("Alamat Email tidak valid!");
+        email.placeholder = "mohon isi alamat email dengan benar";
+        email.focus();
+        return false;
+    }
+
+    if(subject.value = "") {
+        alert("Subjek harus diisi!");
+        subject.placeholder = "mohon isi subjek"
+        subject.focus();
+        return false;
+    }
+
+    if(message == "") {
+        alert("Pesan harus diisi!");
+        message.focus();
+        return false;
+    }
+
+
 }
 
 function isValidEmail(email) {
@@ -23,3 +50,4 @@ function isValidEmail(email) {
     var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
     return emailPattern.test(email);
 }
+
